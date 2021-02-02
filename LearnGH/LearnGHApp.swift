f6498337b7485663a50743bad84273447de61799
hijoDamasco <thumbjiho@gmail.com>
2021-02-02 17:01:47 +0900

    Initial Commit

---
+++ LearnGH/LearnGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGHApp.swift
+//  LearnGH
+//
+//  Created by 엄지호 on 2021/02/02.
+//
+
+import SwiftUI
+
+@main
+struct LearnGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
